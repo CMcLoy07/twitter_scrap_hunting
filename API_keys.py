213aef6776ce6288e476d0b99d51c9144924de1b
Chris McLoy <chrismcloy@Chriss-Air.lan>
2020-10-22 09:14:14 -0600

    Hunting twitter search

---
+++ API_keys.py
@@ -0,0 +1,4 @@
+api_key = 'zXeaTfwggqIsikgyDK8YrJgDN'
+api_key_secret = 'hEVaWCtO7hp6ovX8s40y9AbHOdaBSGhMEKslmy3dOsyxg1he40'
+access_token = "1311364963575844864-QUwXUAIIkZ2hM5uvbLlww4OMJUOCMk"
+access_token_secret = "fIOiZtU4Mpc4eXys0NaVOLCPqc0HjW1PLiq1ItuHNJjTJ"
